7fc1f79a4b350ad9a071f69fcd5e59c55c8ce408
Matt Haggard <haggardii@gmail.com>
2016-11-09 07:44:34 -0700

    Add mkgif.sh

---
+++ mkgif.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+mkdir -p /tmp/election
+
+
+set -x
+# bbc
+mogrify -path /tmp/election/ -crop 800x1150+0+300 +repage images/bbc-*
+
+# cbs
+mogrify -path /tmp/election -crop 1000x1300+0+0 +repage images/cbs-*
+
+# cnn
+# pres
+mogrify -path /tmp/election -crop 487x750+0+329 +repage -format pres.png images/cnn-*
+
+# fox
+mogrify -path /tmp/election -crop 800x979+0+177 +repage images/fox-*
+
+# npr
+# no adjustment
+cp images/npr-* /tmp/election/
+
+# nytimes-house
+mogrify -path /tmp/election -crop 1263x1125+1244+154 +repage images/nytimes-house-*
+
+# nytimes-pres
+mogrify -path /tmp/election -crop 1246x821+1239+322 +repage images/nytimes-pres-*
+
+# nytimes-senate
+mogrify -path /tmp/election -crop 1238x902+1247+201 +repage images/nytimes-senate-*
+
+# politico
+mogrify -path /tmp/election -crop 800x851+0+425 +repage images/politico-*
+
+
+echo "Making gifs..."
+things="npr nytimes-pres nytimes-senate nytimes-house cnn cbs politico fox bbc"
+for prefix in $things; do
+    echo $prefix
+    convert -delay 10 -loop 0 images/${prefix}* gifs/${prefix}.gif
+done
